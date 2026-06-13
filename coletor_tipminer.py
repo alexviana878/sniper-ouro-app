4dce6222e540179becfc164ac860838c569a337f
alexviana878 <147935149+alexviana878@users.noreply.github.com>
2026-06-13 09:42:37 -0300

    Update coletor_tipminer.py

--- coletor_tipminer.py
+++ coletor_tipminer.py
@@ -1,1 +1,65 @@
+# coletor_tipminer.py
+# ==================================================
+# TESTE DE CAPTURA TIPMINER
+# ==================================================
 
+import requests
+from bs4 import BeautifulSoup
+import re
+
+URL = "https://tipminer.com/br/historico/betou/aviator"
+
+def capturar_rodadas():
+
+    try:
+
+        headers = {
+            "User-Agent": "Mozilla/5.0"
+        }
+
+        resposta = requests.get(
+            URL,
+            headers=headers,
+            timeout=15
+        )
+
+        print("STATUS:", resposta.status_code)
+
+        html = resposta.text
+
+        encontrados = re.findall(
+            r'(\d+,\d+)x',
+            html
+        )
+
+        rodadas = []
+
+        for valor in encontrados:
+
+            try:
+                rodadas.append(
+                    float(
+                        valor.replace(",", ".")
+                    )
+                )
+            except:
+                pass
+
+        return rodadas
+
+    except Exception as erro:
+
+        print("ERRO:", erro)
+
+        return []
+
+
+if __name__ == "__main__":
+
+    rodadas = capturar_rodadas()
+
+    print()
+    print("TOTAL CAPTURADAS:", len(rodadas))
+    print()
+
+    print(rodadas[:50])
