c2d2f560b14b4e7d5b1c94fc60810c594d19ccee
alexviana878 <147935149+alexviana878@users.noreply.github.com>
2026-06-13 11:59:39 -0300

    Update coletor_tipminer.py

--- coletor_tipminer.py
+++ coletor_tipminer.py
@@ -10,9 +10,7 @@ import re
 URL = "https://tipminer.com/br/historico/betou/aviator"
 
 def capturar_rodadas():
-
     try:
-
         headers = {
             "User-Agent": "Mozilla/5.0"
         }
@@ -20,10 +18,12 @@ def capturar_rodadas():
         resposta = requests.get(
             URL,
             headers=headers,
-            timeout=15
+            timeout=5
         )
 
+        # --- 📊 PRINTS DE DIAGNÓSTICO AVANÇADO ---
         print("STATUS:", resposta.status_code)
+        print("TAMANHO HTML:", len(resposta.text))
 
         html = resposta.text
 
@@ -35,7 +35,6 @@ def capturar_rodadas():
         rodadas = []
 
         for valor in encontrados:
-
             try:
                 rodadas.append(
                     float(
@@ -48,14 +47,11 @@ def capturar_rodadas():
         return rodadas
 
     except Exception as erro:
-
         print("ERRO:", erro)
-
         return []
 
 
 if __name__ == "__main__":
-
     rodadas = capturar_rodadas()
 
     print()
