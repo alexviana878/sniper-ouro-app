c99646fc552e642cc5b26283833ab93c5a22bb4b
alexviana878 <147935149+alexviana878@users.noreply.github.com>
2026-06-22 10:31:59 -0300

    Update monitor_tipminer.py

--- monitor_tipminer.py
+++ monitor_tipminer.py
@@ -1,1 +1,20 @@
+from playwright.sync_api import sync_playwright
 
+URL = "https://tipminer.com/br/historico/betou/aviator"
+
+with sync_playwright() as p:
+
+    browser = p.chromium.launch(
+        headless=False,
+        channel="chrome"
+    )
+
+    page = browser.new_page()
+
+    page.goto(URL)
+
+    print("TIPMINER ABERTO")
+
+    input("APERTE ENTER PARA FECHAR")
+
+    browser.close()
